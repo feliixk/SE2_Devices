ab39423ada0c43240ca2058f2a1816ebed986842
Felix K <felixkivioja95@gmail.com>
2022-01-05 10:49:49 +0100

    Refactoring to new code base

--- ArduinoComms/src/main/java/InternalServer.java
+++ ArduinoComms/src/main/java/InternalServer.java
@@ -1,55 +1,89 @@
-import com.fazecast.jSerialComm.SerialPort;
+import com.google.gson.Gson;
+import okhttp3.*;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
-import java.io.*;
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
-import java.net.Socket;
 import java.util.ArrayList;
 
 public class InternalServer {
-    public static void main(String[] args) throws IOException {
-        SerialPort[] ports = SerialPort.getCommPorts();
-        ArrayList<String> portsS = new ArrayList<>();
+    private static WebSocket webSocket;
+    private static String SERVER_PATH = "ws://85.197.159.150:1338/arduino";
+    //private static String SERVER_PATH = "ws://85.197.159.131:1337/arduino; //bogges IP
+    private static Gson gson;
+    private static SerialComm serialComm = new SerialComm();
 
-        for (int i = 0; i < ports.length;i++){
-            portsS.add(ports[i].getSystemPortName());
+    public static void main(String[] args) {
+        gson=new Gson();
+        connectWebsocket();
+    }
+
+    private static void connectWebsocket() {
+        OkHttpClient client = new OkHttpClient();
+        System.out.println("connect: " + SERVER_PATH);
+        Request request = new Request.Builder().url(SERVER_PATH).build();
+        webSocket = client.newWebSocket(request, new SocketListener());
+        new Thread(() -> serialComm.reader()).start();
+    }
+    public void SendMessage(String command){ //  när ni ska skicka till oss direkt behöver inte ändras alls.
+        ArrayList<String> info = new ArrayList<>();
+        info.add(command+"-ok"); // måste göra såhär
+        String text123 = gson.toJson(info); // gör om till ett objekt som kan skickas
+        webSocket.send(text123); // skicka  tillbaka
+        System.out.println("Message sent");
+
+    }
+
+    //SocketListener House
+    private static class SocketListener extends WebSocketListener {
+        @Override
+        public void onOpen(WebSocket webSocket, Response response) {
+            super.onOpen(webSocket, response);
+
+            System.out.println("Socket Connection successful");
+            System.out.println("onOpen Thread id: " + Thread.currentThread().getId());
+
         }
-        Socket client = new Socket("85.197.159.131", 2222);
-        BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream()));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
-        ObjectInputStream ois = new ObjectInputStream(client.getInputStream());
-        ObjectOutputStream oos = new ObjectOutputStream(client.getOutputStream());
+        //receive method
+        @Override
+        public void onMessage(WebSocket webSocket, String text) {  //// Det är här ni ska pilla era muppar
+            super.onMessage(webSocket, text); // bry er inte
+            System.out.println("onMessage Method"); // bry er inte
+            System.out.println(webSocket.request()); // bry er inte
 
-        SerialComm sc = new SerialComm();
-        ArrayList<String> responses = new ArrayList<>();
-        SerialPort sPort = SerialPort.getCommPort(portsS.get(0));
-        while (true) {
-            try {
+            ArrayList<String> info  = (ArrayList<String>) gson.fromJson(text,ArrayList.class); // hämtar en arraylist med all info
 
-                ArrayList<String> commandList = (ArrayList<String>) ois.readObject();
-                for(String s : commandList){
-                    System.out.println(s);
-                    String response = sc.sendCommand(sPort,s);
-                    responses.add(response);
-                    Thread.sleep(300);
-                }
-                String ok = "ok";
-                ArrayList<String> temp = new ArrayList<>();
-                temp.add(ok);
-                oos.writeObject(temp);
+            // make loop for bigger list of commands ---------------
+            String[] response = info.get(0).split("-"); // splitar stringer för att få olika detaljer om meddelandet ni behöver inte bry er om detta
 
-//                oos.close();
-//                ois.close();
-//                bw.close();
-//                br.close();
-//                client.close();
+            String command = response[1]; // commandot ni ska använda
+
+            //FelixPlaceholder fp = new FelixPlaceholder();
+            serialComm.sendCommand(command);
+
+            ArrayList<String> responseList = new ArrayList<String>();
+            responseList.add(serialComm.getResponse());
+
+            System.out.println("Response from arduino to server: " + responseList.get(0));
 
 
-            } catch (Exception ex) {
-//                ex.printStackTrace();
+            info.set(0,info.get(0) +"-"+ responseList.get(0)); // omdet går egenom
 
-            }
+            //info.set(0,info.get(0) + "-fail"); // om det inte går egenom
+
+            String text123 = gson.toJson(info); // gör om till ett objekt som kan skickas
+            webSocket.send(text123); // skicka  tillbaka
+            System.out.println("onMessage END");
         }
 
     }
+
+
+//        @Override
+//        public void onFailure(@NotNull WebSocket webSocket, @NotNull Throwable t, @Nullable Response response) {
+//            super.onFailure(webSocket, t, response);
+//            connectWebsocket();
+//        }
+
 }
--- ArduinoComms/src/main/java/SerialComm.java
+++ ArduinoComms/src/main/java/SerialComm.java
@@ -2,11 +2,25 @@ import com.fazecast.jSerialComm.SerialPort;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 
 public class SerialComm {
 
     private   byte[] sendingPack;
     private   byte[] receivingPack;
+    SerialPort port;
+    String response = null;
+    String command = null;
+    Server server = new Server();
+
+    public SerialComm(){
+        SerialPort[] portNames = SerialPort.getCommPorts();
+        ArrayList<String> ports = new ArrayList<>();
+        for (int i = 0; i < portNames.length; i++) {
+            ports.add(portNames[i].getSystemPortName());
+        }
+        port = SerialPort.getCommPort(ports.get(2));
+    }
 
     public void startCon(SerialPort port) throws IOException {
 
@@ -23,11 +37,13 @@ public class SerialComm {
 
             System.out.println(">Comms started, port: " + port.getSystemPortName());
 
+
             port.setBaudRate(9600);
             PrintWriter output = new PrintWriter(port.getOutputStream());
             BufferedInputStream bis = new BufferedInputStream(port.getInputStream());
             BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
             ByteArrayOutputStream buf = new ByteArrayOutputStream();
+
 
 
             InputStream is = null;
@@ -39,6 +55,7 @@ public class SerialComm {
 
 
             try {
+
 
 
                 is = System.in;
@@ -58,7 +75,6 @@ public class SerialComm {
                     byte[] writeContent;
 
                     writeContent = inString.getBytes();
-                    System.out.println(writeContent);
 
                     port.writeBytes(writeContent, 6, 0);
                     output.flush();
@@ -69,36 +85,57 @@ public class SerialComm {
                     } catch (Exception e) {
                     }
 
-                    new Thread(() -> reader(port)).start();
-
 
                 }
             }catch (Exception e) {
             }
 
-            } else{
-                // disconnect from the serial port
-                port.closePort();
-                System.out.println("Conn closed");
-            }
+        } else{
+            // disconnect from the serial port
+            port.closePort();
+            System.out.println("Conn closed");
         }
+    }
 
-    public void reader(SerialPort port){
-        byte[] readBuffer;
+    public void reader(){
         try {
-            while (true)
-            {
+            System.out.println("thread alive");
+            while (true) {
+                byte[] readBuffer;
                 while (port.bytesAvailable() == 0)
-                    Thread.sleep(10);
+                    Thread.sleep(20);
 
-                readBuffer = new byte[port.bytesAvailable()];
-                int numRead = port.readBytes(readBuffer, readBuffer.length);
-                System.out.println("Read " + numRead + " bytes.");
 
-                String s = new String(readBuffer, StandardCharsets.UTF_8);
-                System.out.println("Arduino response: " + s);
+                while (port.bytesAvailable() > 0) {
+                    readBuffer = new byte[port.bytesAvailable()];
+                    int numRead = port.readBytes(readBuffer, readBuffer.length);
+                    System.out.print("Read " + numRead + " bytes. ");
+
+                    String s = new String(readBuffer, StandardCharsets.UTF_8);
+                    System.out.println("Arduino response: " + s);
+                    response = s;
+                }
             }
         } catch (Exception e) { e.printStackTrace(); }
+
+        System.out.println("thread terminated");
+    }
+
+    public String getResponse(){
+        String returnS = null;
+        if(command == null && response != null) {
+            server.SendMessage(response);
+            response = null;
+        }
+        else if(command.equalsIgnoreCase(response)){
+            command = null;
+            returnS =  "ok";
+        }else if(!command.equalsIgnoreCase(response)){
+            returnS =  "not ok";
+        }
+
+        return returnS;
+
     }
 
     public String readCommands(){
@@ -120,7 +157,8 @@ public class SerialComm {
         return readBuffer.toString();
     }
 
-    public String sendCommand(SerialPort port, String command) {
+    public void sendCommand(String command) {
+        this.command = command;
         String response = ""; //response from arduino
         port.setComPortTimeouts(SerialPort.TIMEOUT_SCANNER, 0, 0);
 
@@ -139,16 +177,14 @@ public class SerialComm {
 
         try {
             //send command to arduino
-           // output.print(command);
+            // output.print(command);
             byte[] writeContent;
 
             writeContent = command.getBytes();
             System.out.println(writeContent);
 
-            port.writeBytes(writeContent, 6,0);
+            port.writeBytes(writeContent, 6, 0);
             //output.flush();
-
-
 
 
             //debug sout
@@ -158,40 +194,40 @@ public class SerialComm {
             Thread.sleep(100);
 
             //read response from arduino
-            byte[] contents = new byte[1024];
-            int bytesRead = 0;
-            while (readyToReadResponse) {
-                try {
-                    bytesRead = bis.read(contents);
-                    Thread.sleep(100);
-                    readyToReadResponse = false;
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                response += new String(contents, 0, bytesRead);
-            }
-
-            //debug response printout
-            System.out.println(">Response code: " + response);
-
+//            byte[] contents = new byte[1024];
+//            int bytesRead = 0;
+//            while (readyToReadResponse) {
+//                try {
+//                    bytesRead = bis.read(contents);
+//                    Thread.sleep(100);
+//                    readyToReadResponse = false;
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//                response += new String(contents, 0, bytesRead);
+//            }
+//
+//            //debug response printout
+//            System.out.println(">Response code: " + response);
+//
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        return response; // return response from arduino to server
+//        return response; // return response from arduino to server
     }
 
     public static void sendData(SerialPort arduinoPort, byte[] buffer){
         byte[] sendingPack = new byte[6];
         byte[] receivingPack= new byte[36];
 
-            arduinoPort.writeBytes(sendingPack,6,0);
+        arduinoPort.writeBytes(sendingPack,6,0);
 
-            //System.out.println("Sending"+bytesToHexString(sendingPack));
-            try {
-                Thread.sleep(200);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+        //System.out.println("Sending"+bytesToHexString(sendingPack));
+        try {
+            Thread.sleep(200);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
+}
