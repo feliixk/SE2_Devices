bdab3b9a0a9764384737b6ac7d3790ad170016ab
Felix K <felixkivioja95@gmail.com>
2022-01-10 17:26:19 +0100

    Final commit

--- ArduinoComms/src/main/java/InternalServer.java
+++ ArduinoComms/src/main/java/InternalServer.java
@@ -16,10 +16,13 @@ public class InternalServer {
     private static SerialComm serialComm = new SerialComm();
 
     public static void main(String[] args) {
-        gson=new Gson();
+        gson = new Gson();
         connectWebsocket();
+        serialComm.sendCommand("a2");
+        serialComm.sendCommand("a0");
     }
 
+    //Connecting to external webserver
     private static void connectWebsocket() {
         OkHttpClient client = new OkHttpClient();
         System.out.println("connect: " + SERVER_PATH);
@@ -28,85 +31,92 @@ public class InternalServer {
         new Thread(() -> serialComm.reader()).start();
         new Thread(() -> threadedTasks()).start();
     }
-    public void SendMessage(String command){ //  när ni ska skicka till oss direkt behöver inte ändras alls.
-        ArrayList<String> info = new ArrayList<>();
-        if(command.contains("Alarm")) {
-            info.add(command + "-alarm"); // måste göra såhär
-        }else if (command.contains("a0")||command.contains("a2")){
-            info.add(command.substring(0,2)+"-"+command.substring(2));
-        }else if (command.contains("p")){
-            info.add(command.substring(1));
-        }
-        String text123 = gson.toJson(info); // gör om till ett objekt som kan skickas
-        webSocket.send(text123); // skicka  tillbaka
-        System.out.println("Message sent");
 
+    //Method for sending messages from Arduino to external server
+    public void SendMessage(String command) {
+        ArrayList<String> info = new ArrayList<>();
+        if (command.contains("Alarm") || command.equalsIgnoreCase("wLeakage")) {
+            info.add(command + "-alarm"); // måste göra såhär
+        } else if (command.contains("a0") || command.contains("a2")) {
+            info.add(command.substring(0, 2) + "-" + command.substring(2));
+        } else if (command.contains("p")) {
+            info.add(command.substring(1));
+        } else {
+            info.add(command + "-ok");
+        }
+        String text123 = gson.toJson(info);
+        webSocket.send(text123);
+        System.out.println(">[sendMessage] Command: '" + text123 + "' sent to server!");
     }
 
-    //SocketListener House
+    //SocketListener House, listen on external server
     private static class SocketListener extends WebSocketListener {
         @Override
         public void onOpen(WebSocket webSocket, Response response) {
             super.onOpen(webSocket, response);
 
-            System.out.println("Socket Connection successful");
-            System.out.println("onOpen Thread id: " + Thread.currentThread().getId());
+            System.out.println(">Socket Connection successful!");
 
         }
+
         //receive method
         @Override
-        public void onMessage(WebSocket webSocket, String text) {  //// Det är här ni ska pilla era muppar
+        public void onMessage(WebSocket webSocket, String text) {
             super.onMessage(webSocket, text); // bry er inte
-            System.out.println("onMessage Method"); // bry er inte
-            System.out.println(webSocket.request()); // bry er inte
+            System.out.println("<------------------------------->");
+            System.out.println(">onMessage Method entered");
+            System.out.println(webSocket.request());
 
-            ArrayList<String> info  = (ArrayList<String>) gson.fromJson(text,ArrayList.class); // hämtar en arraylist med all info
+            ArrayList<String> info = (ArrayList<String>) gson.fromJson(text, ArrayList.class);
 
             // make loop for bigger list of commands ---------------
-            String[] response = info.get(0).split("-"); // splitar stringer för att få olika detaljer om meddelandet ni behöver inte bry er om detta
+            String[] response = info.get(0).split("-");
 
-            String command = response[1]; // commandot ni ska använda
+            String command = response[1];
 
-            //FelixPlaceholder fp = new FelixPlaceholder();
+            //response[0];
+            if (command.startsWith("p")) {
+                if (command.length() == 2) {
+                    command = "p00" + command.substring(1);
+                } else if (command.length() == 3) {
+                    command = "p0" + command.substring(2);
+                }
+            }
             serialComm.sendCommand(command);
 
             ArrayList<String> responseList = new ArrayList<String>();
+
             responseList.add(serialComm.getResponse());
 
             System.out.println("Response from arduino to server: " + responseList.get(0));
-            if(command.contains("p")){
-                info.set(0,info.get(0)+"-"+command.substring(1));
-            }else {
-                info.set(0, info.get(0) + "-" + responseList.get(0)); // omdet går egenom
+            if (command.contains("p") || command.contains("t")) {
+                info.set(0, info.get(0) + "-" + command.substring(1));
+            } else {
+                info.set(0, info.get(0) + "-" + responseList.get(0));
             }
-
-            //info.set(0,info.get(0) + "-fail"); // om det inte går egenom
 
             String text123 = gson.toJson(info); // gör om till ett objekt som kan skickas
             webSocket.send(text123); // skicka  tillbaka
-            System.out.println("onMessage END");
         }
 
     }
-    public static void threadedTasks(){
-        int pingTime = LocalTime.now().getSecond()+15;
-        while(true) {
-            if (LocalTime.now().getSecond() == pingTime) {
-                pingTime = LocalTime.now().getSecond() + 15;
-                if(pingTime>59){
-                    pingTime-=60;
+
+    // Automatic tasks to read temp and electricity consumption with polling
+    private static void threadedTasks() {
+        System.out.println(">Timer thread alive"
+                );
+        int pingTime = LocalTime.now().getMinute() + 5;
+        while (true) {
+            if (LocalTime.now().getMinute() == pingTime||LocalTime.now().getMinute()==pingTime+1) {
+                System.out.println(">Sending temp & el auto");
+                pingTime = LocalTime.now().getMinute() + 5;
+                if (pingTime > 59) {
+                    pingTime -= 60;
                 }
                 serialComm.sendCommand("a2");
                 serialComm.sendCommand("a0");
             }
         }
     }
-
-
-//        @Override
-//        public void onFailure(@NotNull WebSocket webSocket, @NotNull Throwable t, @Nullable Response response) {
-//            super.onFailure(webSocket, t, response);
-//            connectWebsocket();
-//        }
 
 }
--- ArduinoComms/src/main/java/SerialComm.java
+++ ArduinoComms/src/main/java/SerialComm.java
@@ -11,6 +11,7 @@ public class SerialComm {
     String command = null;
     InternalServer server = new InternalServer();
 
+    //Initializing serial communication interface
     public SerialComm() {
         try {
             SerialPort[] portNames = SerialPort.getCommPorts();
@@ -24,194 +25,116 @@ public class SerialComm {
         }
     }
 
-    public void startCon(SerialPort port) throws IOException {
-
-        String strFileContents = "";
-
-        port.setComPortTimeouts(SerialPort.TIMEOUT_SCANNER, 0, 0);
-
-        if (port.openPort()) {
-            // wait after connecting, so arduino/xbee bootloader can finish
-            try {
-                Thread.sleep(100);
-            } catch (Exception e) {
-            }
-
-            System.out.println(">Comms started, port: " + port.getSystemPortName());
-
-
-            port.setBaudRate(9600);
-            PrintWriter output = new PrintWriter(port.getOutputStream());
-            BufferedInputStream bis = new BufferedInputStream(port.getInputStream());
-            BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-            ByteArrayOutputStream buf = new ByteArrayOutputStream();
-
-
-            InputStream is = null;
-            BufferedReader br = null;
-            //output.print("p000");
-
-
-            try {
-
-
-                is = System.in;
-                br = new BufferedReader(new InputStreamReader(is));
-
-                String inString;
-                while ((inString = br.readLine()) != null) {
-                    if (inString.equalsIgnoreCase(("disconnect"))) {
-                        break;
-                    }
-
-
-                    System.out.println("> Sent command " + inString + " to Arduino");
-
-
-                    //uncomment this, when trying with arduino IRL
-                    byte[] writeContent;
-
-                    writeContent = inString.getBytes();
-
-                    port.writeBytes(writeContent, 6, 0);
-                    output.flush();
-
-
-                    try {
-                        Thread.sleep(100);
-                    } catch (Exception e) {
-                    }
-
-
-                }
-            } catch (Exception e) {
-            }
-
-        } else {
-            // disconnect from the serial port
-            port.closePort();
-            System.out.println("Conn closed");
-        }
-    }
-
+    //Method that runs in a separate thread to read all incoming messages from the Arduino
     public void reader() {
         try {
             System.out.println(">Reader thread alive");
             while (true) {
-                byte[] readBuffer;
-                while (port.bytesAvailable() == 0)
-                {Thread.sleep(15);}
+                byte[] readBuffer = null;
+                while (port.bytesAvailable() == 0) {
+                    Thread.sleep(150);
+                }
 
 
                 while (port.bytesAvailable() > 0) {
                     readBuffer = new byte[port.bytesAvailable()];
                     int numRead = port.readBytes(readBuffer, readBuffer.length);
                     System.out.print("Read " + numRead + " bytes. ");
-
+                }
+                if (readBuffer != null) {
                     String s = new String(readBuffer, StandardCharsets.UTF_8);
                     System.out.println("Arduino response: " + s);
                     response = s;
-                    //getResponse();
 
-                    if(s.contains("Alarm")){
+                    if (s.contains("Alarm")) {
                         server.SendMessage(s);
                         System.out.println("Sending alarm to SERVER");
-                    }else if(s.startsWith("a0")||s.startsWith("a2")){
+                    } else if (s.contains("wLeakage")) {
+                        server.SendMessage(s);
+                        System.out.println("Sending water alarm to SERVER");
+                    } else if (s.equalsIgnoreCase("m0110") || s.equalsIgnoreCase("m1110")) {
+                        server.SendMessage(s);
+                    } else if (s.startsWith("a0") || s.startsWith("a2")) {
                         double t = Double.parseDouble(s.substring(2));
 //                       int f =(int)Math.round(t);
-                        int f = (int)(t*10);
-                       s=s.substring(0,2);
-                       s+=String.valueOf(f);
-                        System.out.println(s+"    --");
+                        int f = (int) (t * 10);
+                        s = s.substring(0, 2);
+                        s += String.valueOf(f);
                         server.SendMessage(s);
+                    } else if (s.equalsIgnoreCase("p000")) {
+                        s.substring(2);
+                    } else if (s.equalsIgnoreCase("m0111")) {
+                        System.out.println("Sending out light ON");
+                        server.SendMessage("m0111");
+                    } else if (s.equalsIgnoreCase("m1111")) {
+                        server.SendMessage("m1111");
+                        System.out.println("Sending out light OFF");
                     }
+
+
                 }
             }
         } catch (Exception e) {
-            System.out.println("server seems to be offline");
+            System.out.println("Error, no active connection to smart house");
         }
 
-        System.out.println("thread terminated");
+        System.out.println(">reader thread terminated");
     }
 
+    // Method for getting responses from the Arduino
     public String getResponse() {
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         String returnS = null;
         if (command == null && response != null) {
             server.SendMessage(response);
             response = null;
-            System.out.println("1");
-        } else if (command.equalsIgnoreCase(response)) {
+            System.out.println("Alarm OR Automatic lighting sent!");
+        }
+        if (command.equalsIgnoreCase(response)) {
             command = null;
             returnS = "ok";
-            System.out.println("2");
+            System.out.println("Ordinary command sent");
         } else if (!command.equalsIgnoreCase(response)) {
             returnS = "not ok";
-            System.out.println("3");
+            System.out.println("not ok response");
         }
 
         return returnS;
 
     }
 
+    //Method for sending commands to Arduino
     public void sendCommand(String command) {
         this.command = command;
-        String response = ""; //response from arduino
         port.setComPortTimeouts(SerialPort.TIMEOUT_SCANNER, 0, 0);
         port.setBaudRate(9600);
 
         if (port.openPort()) {
-            // wait after connecting, so arduino/xbee bootloader can finish
             try {
                 Thread.sleep(100);
             } catch (Exception e) {
             }
         }
-        // get streams from serial port
-        PrintWriter output = new PrintWriter(port.getOutputStream());
-        BufferedInputStream bis = new BufferedInputStream(port.getInputStream());
-
-        boolean readyToReadResponse = true;
-
         try {
             //send command to arduino
-            // output.print(command);
             byte[] writeContent;
-
             writeContent = command.getBytes();
             System.out.println(writeContent);
-
             port.writeBytes(writeContent, 6, 0);
-            //output.flush();
-
 
             //debug sout
             System.out.println("> Sent command " + command + " to Arduino");
 
-            // wait 100 ms for response.
-            Thread.sleep(100);
 
-            //read response from arduino
-//            byte[] contents = new byte[1024];
-//            int bytesRead = 0;
-//            while (readyToReadResponse) {
-//                try {
-//                    bytesRead = bis.read(contents);
-//                    Thread.sleep(100);
-//                    readyToReadResponse = false;
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//                response += new String(contents, 0, bytesRead);
-//            }
-//
-//            //debug response printout
-//            System.out.println(">Response code: " + response);
-//
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-//        return response; // return response from arduino to server
     }
 
 }
